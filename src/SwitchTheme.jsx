355b4a6b5ba8a4ae77732f4d3e76caa59b747c54
Anton Knysh <anton@profairgames.com>
2024-03-15 14:27:47 +0200

    refactoring

--- src/SwitchTheme.jsx
+++ src/SwitchTheme.jsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
 import useLocalStorage from "./hooks/useLocalStorage";
 
-export default function App() {
+export default function SwitchTheme() {
   const [theme, setTheme] = useLocalStorage("theme", "dark");
 
-  const toggleSwitchTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light");
+  const toggleTheme = () => {
+    theme === "dark" ? setTheme("light") : setTheme("dark");
   }
 
   return (
     <div className="main-container" data-theme={theme}>
       <div className="container">
         <h1 className="header">Hello, There!</h1>
-        <button className="switch-mode-btn" onClick={toggleSwitchTheme}>SWITCH MODE</button>
+        <button className="switch-mode-btn" onClick={toggleTheme}>SWITCH MODE</button>
       </div>
     </div>
   );
--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -1,23 +1,20 @@
 import { useEffect, useState } from "react";
 
 export default function useLocalStorage (key, defaultValue) {
-  const [value, setValue] = useState(() => {
-    let currentValue;
-    try {
-      currentValue = JSON.parse(localStorage.getItem(key) || String(defaultValue));
-      console.log(currentValue);
-    } catch (e) {
-      console.log(e);
-      currentValue = defaultValue;
-    }
+ const [value, setValue] = useState(() => {
+  let currentValue;
+  try {
+    currentValue = JSON.parse(localStorage.getItem(key) || String(defaultValue));
+  }catch(e) {
+    console.log(e);
+    currentValue = defaultValue;
+  }
+  return currentValue;
+ });
 
-    return currentValue;
-  });
+ useEffect(() => {
+  localStorage.setItem(key, JSON.stringify(value));
+ },[value]);
 
-  useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
-    console.log('value-2 >>>>', value);
-  },[value]);
-  console.log('value-3 >>>>', value);
-  return [value, setValue];
+ return [value, setValue];
 }
